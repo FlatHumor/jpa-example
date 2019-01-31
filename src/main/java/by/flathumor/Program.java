de97f8419bcd78dff90371a1771e8245b65f3b9c
FlatHumor <eatenbrains@gmail.com>
2019-01-31 23:55:54 +0300

    Added namedquery

--- src/main/java/by/flathumor/Program.java
+++ src/main/java/by/flathumor/Program.java
@@ -3,6 +3,8 @@ package by.flathumor;
 import by.flathumor.entity.User;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 public class Program
 {
@@ -17,6 +19,11 @@ public class Program
         user.setPassword("12345");
         manager.persist(user);
         manager.getTransaction().commit();
+
+        Query query = manager.createNamedQuery("User.findAll");
+        List<User> users = query.getResultList();
+        for (User u : users)
+            System.out.println("Username" + u.getUsername());
         manager.clear();
 
         System.out.println("Record Successfully Inserted In The Database");
